dc49a611227222302b7b9d14030e56c39e2e8afb
himnie <almorabitibrahim@gmail.com>
2025-04-10 18:43:50 +0200

    actua-10-04-25

---
+++ python_inicial/hello.py
@@ -0,0 +1,12 @@
+# Este es un programa básico en Python
+
+# Imprime un mensaje en pantalla
+print("Hello, World!")
+
+# Pregunta tu nombre
+name = input("¿Cómo te llamas? ")
+age = input("¿que edad tienes? ")
+
+# Saluda con el nombre
+print(f"¡Hola, {name}! Bienvenido a Python 😄")
+print(f"¡Hola ibrahim tu edad es : {age}! Bienvenido a Python 😄")
